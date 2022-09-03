03972f7297a59a70f0b6ac3d916425d02c93da83
Elijah Detilisi <Elijahdetilisi@gmail.com>
2022-09-03 22:51:21 +0200

    add vocabulary

---
+++ George.Services.Layer/AudioService/Vocabulary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace George.Services.Layer.AudioService
+{
+    public static class Vocabulary
+    {
+        #region Data fields
+        private static Dictionary<string, string> _Prompts = new Dictionary<string, string>();
+        #endregion
+
+        //_Prompts = new Dictionary<string, string>();
+
+    }
+}
